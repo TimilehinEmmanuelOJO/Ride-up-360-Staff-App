0e8d03d1d36abc997891f86d51a3ddf6e50b4329
timi ojo <timilehin175@gmail.com>
2020-08-21 16:39:39 +0100

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(RideHub());
 }
 
+class RideHub extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: Container(
+            constraints: BoxConstraints.expand(),
+            decoration: BoxDecoration(
+              image: DecorationImage(image: AssetImage('images/downloads.jpg'),
+              fit: BoxFit.cover),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

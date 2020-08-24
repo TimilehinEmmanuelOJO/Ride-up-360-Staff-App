823649c05181838c4b2b8e76609a321a24444774
timi ojo <timilehin175@gmail.com>
2020-08-24 18:07:47 +0100

    .

---
+++ lib/Page2.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+
+class Page2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: RaisedButton(onPressed: () {},
+        child: Text('page two'),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:ridehub360_staff_app/Page2.dart';
 
 void main() {
   runApp(RideHub());
@@ -12,18 +11,48 @@ class RideHub extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: new BoxDecoration(
-            color: Colors.black,
-            image: new DecorationImage(
-              fit: BoxFit.cover,
-              colorFilter: ColorFilter.mode(Colors.blue.withOpacity(0.7), BlendMode.dstATop),
-              image: AssetImage('images/IMG_0792.JPG'),
-              //TODO: ensure you put a container on the image and a text which says welcome.
-            ),),
+        body: SafeArea(
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.black,
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                colorFilter: ColorFilter.mode(
+                    Colors.blue.withOpacity(0.7), BlendMode.dstATop),
+                image: NetworkImage('https://cdn.dribbble.com/users/383120/screenshots/4934356/bicycle_drib.png'),
+                //TODO: ensure you put a container on the image and a text which says welcom e.
+              ),
+            ),
+              child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  SizedBox(height: 20,),
+                  Image(image: AssetImage('images/ridehub360.png')),
+                  SizedBox(
+                    height: 500,
+                  ),
+                  Row(
+                    children: <Widget>[
+                      SizedBox(
+                        width: 100,
+                      ),
+                      Container(
+                        height: 60,
+                        width: 200,
+                        decoration: BoxDecoration(
+                          color: Colors.white12.withOpacity(0.0),border: Border.all(color: Colors.white),
+                            borderRadius: BorderRadius.all(Radius.circular(30))),
+                        child: Center(
+                          child: Text('WELCOME',
+                          style: TextStyle(color: Colors.white,fontSize: 30,fontWeight: FontWeight.bold,fontStyle: FontStyle.italic),),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
         ),
-           ),
       );
   }
 }
-

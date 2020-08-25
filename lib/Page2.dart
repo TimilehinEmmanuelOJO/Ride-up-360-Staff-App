fbb5eb85b57bd05fb3550c122b717d370cc9e102
timi ojo <timilehin175@gmail.com>
2020-08-25 17:13:44 +0100

    Page 2

--- lib/Page2.dart
+++ lib/Page2.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
-
-
+import 'package:ridehub360_staff_app/main.dart';
 
 class Page2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(appBar: AppBar(backgroundColor: Colors.yellow,),
-      body: Container(
-        width: 300,height: 400, color: Colors.grey,
-      ),),
+      home: MyHome(),
+    );
+  }
+}
 
+
+class MyHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+      body:Container(
+        decoration: BoxDecoration( image: DecorationImage(
+          fit: BoxFit.cover,
+          image: AssetImage('images/ride3.JPG')
+        )),
+      )
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -20,59 +20,56 @@ class MyHome extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: SafeArea(
-          child: Container(
-            decoration: BoxDecoration(
-              color: Colors.black,
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                colorFilter: ColorFilter.mode(
-                    Colors.blue.withOpacity(0.7), BlendMode.dstATop),
-                image: NetworkImage('https://cdn.dribbble.com/users/383120/screenshots/4934356/bicycle_drib.png'),
-                //TODO: ensure you put a container on the image and a text which says welcom e.
-              ),
+        body: Container(
+          decoration: BoxDecoration(
+            color: Colors.black,
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              colorFilter: ColorFilter.mode(
+                  Colors.blue.withOpacity(0.7), BlendMode.dstATop),
+              image: AssetImage('images/ride.jpg'),
+              //TODO: ensure you put a container on the image and a text which says welcom e.
             ),
-              child: Column(crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  SizedBox(height: 60,),
-                  Row(
-                    children: <Widget>[
-                      SizedBox(width: 20,),
-                      Image(image: AssetImage('images/ridehub360.png')),
-                      Column(children: <Widget>[
-                        SizedBox(height: 45,),
-                        Text('IDEHUB360.COM',
-                          style: TextStyle(color: Colors.grey,fontSize: 25,fontWeight: FontWeight.bold,),),
-                      ],
-                      ),
+          ),
+            child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(height: 10,),
+                Row(
+                  children: <Widget>[
+                    SizedBox(width: 20,),
+                    Image(image: AssetImage('images/ridehub360.png'),width: 60,),
+                    Column(children: <Widget>[
+                      SizedBox(height: 100,),
                     ],
-                  ),
-                  SizedBox(
-                    height: 500,
-                  ),
-                  Row(
-                    children: <Widget>[
-                      SizedBox(
-                        width: 100,
-                      ),
-                      FlatButton(onPressed: () => Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => Page2()),),
-                        child: Container(
-                          height: 60,
-                          width: 200,
-                          decoration: BoxDecoration(
-                            color: Colors.white12.withOpacity(0.0),border: Border.all(color: Colors.white),
-                              borderRadius: BorderRadius.all(Radius.circular(30))),
-                          child: Center(
-                            child: Text('WELCOME',
-                            style: TextStyle(color: Colors.white,fontSize: 30,fontWeight: FontWeight.bold,fontStyle: FontStyle.italic),),
-                          ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 550,
+                ),
+                Row(
+                  children: <Widget>[
+                    SizedBox(
+                      width: 100,
+                    ),
+                    FlatButton(onPressed: () => Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => Page2()),),
+                      // TODO: next page connected here
+                      child: Container(
+                        height: 60,
+                        width: 200,
+                        decoration: BoxDecoration(
+                          color: Colors.white12.withOpacity(0.0),border: Border.all(color: Colors.white),
+                            borderRadius: BorderRadius.all(Radius.circular(30))),
+                        child: Center(
+                          child: Text('WELCOME',
+                          style: TextStyle(color: Colors.white,fontSize: 30,fontWeight: FontWeight.bold,fontStyle: FontStyle.italic),),
                         ),
                       ),
-                    ],
-                  )
-                ],
-              ),
+                    ),
+                  ],
+                )
+              ],
             ),
           ),
         ),
